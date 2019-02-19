187d0aff6cf7c6b744a46cabfaa2d236220a8f77
ElRaptorus <christian.werner@5minds.de>
2019-02-19 12:44:38 +0100

    :fire: Remove singleton declarations

--- ioc_module.js
+++ ioc_module.js
@@ -3,8 +3,7 @@ const {CorrelationService} = require('./dist/commonjs/index');
 function registerInContainer(container) {
   container
     .register('CorrelationService', CorrelationService)
-    .dependencies('CorrelationRepository', 'IamService', 'ProcessDefinitionRepository')
-    .singleton();
+    .dependencies('CorrelationRepository', 'IamService', 'ProcessDefinitionRepository');
 }
 
 module.exports.registerInContainer = registerInContainer;
